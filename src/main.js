c84dab73f94fd8da2aa5857e79fe8d8f28fe5da5
liuyongjie <liuyj@chances.com.cn>
2024-12-03 18:54:46 +0800

    fix(依赖): 修复依赖错误的问题

--- src/main.js
+++ src/main.js
@@ -1,6 +1,6 @@
 // src/main.js
 import { createApp } from 'vue';
-import App from './app.vue';
+import App from './App.vue';
 import router from './router';
 import store from './store';
 import './assets/styles/app.css';
